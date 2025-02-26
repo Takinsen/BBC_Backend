5b7b05fac5bd5366647c33df1ebfb1190df94d71
Pulaphew <pufagamer@gmail.com>
2025-02-26 14:16:40 +0700

    Implements Authentication(routes,middlewares,controllers) And Models(Account,Hotel)

--- models/Hotel.js
+++ models/Hotel.js
@@ -11,29 +11,31 @@ const hotelSchema = new mongoose.Schema({
         required: [true, 'Please add a telephone number'],
         match: [/^\d{10}$/, 'Telephone number must be exactly 10 digits']
     },
-    province: {
-        type: String,
-        required: [true, 'Please add a province']    
-    },
-    district: {
-        type: String,
-        required: [true, 'Please add a district']
-    },
-    street: {
-        type: String,
-        required: [true, 'Please add a street']
+    address: {
+        province: {
+            type: String,
+            required: [true, 'Please add a province']
+        },
+        district: {
+            type: String,
+            required: [true, 'Please add a district']
+        },
+        street: {
+            type: String,
+            required: [true, 'Please add a street']
+        },
+        zipcode: {
+            type: String,
+            required: [true, 'Please add a postal'] ,
+            match: [/^\d{1,5}$/, 'Zipcode must be at most 5 digits']
+        }
     },
     location_number: {
         type: Number,
         required: [true, 'Please add a number'],
         min: [1, 'location_number must be at least 1'],
         max: [999, 'location_number must be at most 999']
-    },
-    zipcode: {
-        type: String,
-        required: [true, 'Please add a postal'] ,
-        match: [/^\d{1,5}$/, 'Zipcode must be at most 5 digits']
-    },
+    }
 });
 
 const Hotel = mongoose.model('Hotel', hotelSchema);
