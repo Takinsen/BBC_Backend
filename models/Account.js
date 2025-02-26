0caf3ea8a9cedcd1822a98b02c45b7c41f13a6b8
Pulaphew <pufagamer@gmail.com>
2025-02-26 15:36:05 +0700

    Fix Account Email Format

--- models/Account.js
+++ models/Account.js
@@ -21,14 +21,14 @@ const accountSchema = new mongoose.Schema({
         required: [true, 'Please add an email'],
         unique: true,
         match: [
-            /^(([^<>()[]\.,;:\s@"]+(.[^<>()[]\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z0-9]+.)+[a-zA-Z]{2,}))$/,
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z0-9]+\.)+[a-zA-Z]{2,}))$/,
             'Please add a valid email'
         ]
     },
     password: {
         type: String,
         required: [true, 'Please add a password'],
-        minlength: 6,  // Fixed typo here
+        minlength: 6,
         select: false
     },
     role: { 
